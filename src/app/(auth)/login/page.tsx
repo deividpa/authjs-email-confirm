8d499fb46321e31be8a95b43811dff2ac0993ffd
David Pérez <deividpa022@gmail.com>
2025-02-23 20:23:06 -0500

    feat: add LoginPage component with form login integration

---
+++ src/app/(auth)/login/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import FormLogin from '@/components/form-login'
+
+const LoginPage = () => {
+  return (
+    <div>
+      <FormLogin />
+    </div>
+  )
+}
+
+export default LoginPage
