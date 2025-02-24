5e7458abff87e0886c768ddd3219d43be0a863ea
David Pérez <deividpa022@gmail.com>
2025-02-23 20:23:11 -0500

    feat: add RegisterPage component with form registration integration

---
+++ src/app/(auth)/register/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import FormRegister from '@/components/form-register'
+
+const RegisterPage = () => {
+  return (
+    <div>
+      <FormRegister />
+    </div>
+  )
+}
+
+export default RegisterPage
