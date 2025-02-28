2429cda06b55c7224657928b33e77950ff50ff0a
David Pérez <deividpa022@gmail.com>
2025-02-28 15:40:05 -0500

    feat: enhance registration form with error handling and loading state management

--- src/components/form-register.tsx
+++ src/components/form-register.tsx
@@ -1,7 +1,8 @@
 "use client";
 
-import React from "react";
+import React, { useState, useTransition } from "react";
 import { useForm } from "react-hook-form";
+import { useRouter } from "next/navigation";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
 import { registerSchema } from "@/lib/zod";
@@ -15,10 +16,16 @@ import {
   FormControl,
   FormMessage,
 } from "@/components/ui/form";
+import { registerAction } from "../../actions/auth-action";
 
 type RegisterFormInputs = z.infer<typeof registerSchema>;
 
 const FormRegister = () => {
+
+  const [error, setError] = useState<string | null>(null);
+  const [isPending, startTransition] = useTransition();
+  const router = useRouter();
+
   const form = useForm<RegisterFormInputs>({
     resolver: zodResolver(registerSchema),
     defaultValues: {
@@ -30,80 +37,96 @@ const FormRegister = () => {
   });
 
   const onSubmit = (data: RegisterFormInputs) => {
-    console.log(data);
+    setError(null);
+      startTransition(async() => {
+        const response = await registerAction(data);
+        console.log(response);
+        if(response.error) {
+          setError(response.error);
+        } else {
+          setError(null);
+          router.push('/dashboard');
+        }
+      });
   };
 
   return (
-    <Form {...form}>
-      <form
-        onSubmit={form.handleSubmit(onSubmit)}
-        className="max-w-md mx-auto p-4 space-y-6"
-      >
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Email</FormLabel>
-              <FormControl>
-                <Input type="email" placeholder="Ingresa tu email" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="name"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Nombre</FormLabel>
-              <FormControl>
-                <Input type="text" placeholder="Ingresa tu nombre" {...field} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="password"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Contraseña</FormLabel>
-              <FormControl>
-                <Input
-                  type="password"
-                  placeholder="Ingresa tu contraseña"
-                  {...field}
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name="confirmPassword"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Confirmar Contraseña</FormLabel>
-              <FormControl>
-                <Input
-                  type="password"
-                  placeholder="Confirma tu contraseña"
-                  {...field}
-                />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit" className="w-full">
-          Registrarse
-        </Button>
-      </form>
-    </Form>
+    <div>
+      <h1 className="text-3xl font-bold mb-4 text-slate-600">Registro</h1>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="max-w-md mx-auto p-4 space-y-6"
+        >
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Email</FormLabel>
+                <FormControl>
+                  <Input type="email" placeholder="Ingresa tu email" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="name"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Nombre</FormLabel>
+                <FormControl>
+                  <Input type="text" placeholder="Ingresa tu nombre" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Contraseña</FormLabel>
+                <FormControl>
+                  <Input
+                    type="password"
+                    placeholder="Ingresa tu contraseña"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="confirmPassword"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Confirmar Contraseña</FormLabel>
+                <FormControl>
+                  <Input
+                    type="password"
+                    placeholder="Confirma tu contraseña"
+                    {...field}
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          {
+            error && <FormMessage>{error}</FormMessage>
+          }
+          <Button type="submit" className="w-full" disabled={isPending}>
+            Registrarse
+          </Button>
+        </form>
+      </Form>
+    </div>
   );
 };
 
