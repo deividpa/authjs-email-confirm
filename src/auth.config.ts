d29247e5c229c73069e6127df0ac786c10e6c03b
David Pérez <deividpa022@gmail.com>
2025-02-28 14:17:06 -0500

    feat: enhance authentication logic to validate user credentials and passwords

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,22 +1,39 @@
 import type { NextAuthConfig } from "next-auth";
 import Credentials from "next-auth/providers/credentials"
+import { loginSchema } from "./lib/zod";
+import { prisma } from "./lib/prisma";
+import bcrypt from "bcryptjs";
 
 const authConfig: NextAuthConfig = {
   providers: [
     Credentials({
       authorize: async (credentials) => {
-        console.log({credentials});
+        
+        const { data, success } = loginSchema.safeParse(credentials);
 
-        if(credentials.email !== "test@test.com") {
-          throw new Error("Invalid email");
+        if (!success) {
+          throw new Error("Invalid credentials");
         }
 
-        // return user object with their profile data
-        return {
-          id: "1",
-          name: "John Doe",
-          email: "test@test.com"
+        // Check if the email exists in the database
+        const user = await prisma.user.findUnique({
+          where: {
+            email: data.email
+          },
+        });
+
+        if (!user || !user.password) {
+          throw new Error("No user found with this email");
         }
+
+        // Check if the password is correct
+        const isValid = await bcrypt.compare(data.password, user.password);
+
+        if (!isValid) {
+          throw new Error("Incorrect password");
+        }
+
+        return user;
       },
     }),
   ],
