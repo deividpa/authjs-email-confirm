0d969bc0a66af0e17023b2dcf8e80da5f595a13d
David Pérez <deividpa022@gmail.com>
2025-03-04 00:39:52 -0500

    feat: add email verification token generation for unverified users

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -3,6 +3,7 @@ import Credentials from "next-auth/providers/credentials"
 import { loginSchema } from "./lib/zod";
 import { prisma } from "./lib/prisma";
 import bcrypt from "bcryptjs";
+import { nanoid } from "nanoid";
 
 const authConfig: NextAuthConfig = {
   providers: [
@@ -31,6 +32,37 @@ const authConfig: NextAuthConfig = {
 
         if (!isValid) {
           throw new Error("Incorrect password");
+        }
+
+        // Email Verification
+        if (!user.emailVerified) {
+
+          const verifyEmailToken = await prisma.verificationToken.findFirst({
+            where: {
+              identifier: user.email,
+            },
+          });
+
+          // Delete the token if it exists
+          if (verifyEmailToken?.identifier) {
+            await prisma.verificationToken.delete({
+              where: {
+                identifier: user.email,
+              },
+            });
+          }
+
+          const token = nanoid();
+
+          // Create a new token
+          await prisma.verificationToken.create({
+            data: {
+              identifier: user.email,
+              token,
+              expires: new Date(Date.now() + 1000 * 60 * 60 * 24), // 24 hours
+            },
+          });
+
         }
 
         return user;
