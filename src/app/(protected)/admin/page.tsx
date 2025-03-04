1a7aa1812e07a160887a078caff75ce21636efbc
David Pérez <deividpa022@gmail.com>
2025-03-03 23:58:27 -0500

    feat: implement admin page session validation and role-based access control

--- src/app/(protected)/admin/page.tsx
+++ src/app/(protected)/admin/page.tsx
@@ -1,8 +1,26 @@
+import { auth } from '@/auth'
+import LogoutButton from '@/components/logout-button';
 import React from 'react'
 
-const AdminPage = () => {
+const AdminPage = async () => {
+
+  const session = await auth();
+
+  if(!session) {
+    return null;
+  }
+
+  if(session?.user?.role !== 'ADMIN') {
+    return (
+      <div>You are not an admin</div>
+  );
+  }
+
   return (
-    <div>Admin Page</div>
+    <div className="container">
+      <LogoutButton />
+      <pre>{JSON.stringify(session, null, 2)}</pre>
+    </div>
   )
 }
 
